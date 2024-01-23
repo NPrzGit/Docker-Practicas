9af02f8bd1534b3141b0162d047df96895e149b5
Nery Perez <alexander99.np@gmail.com>
2024-01-23 16:36:41 -0600

    Segundo script con argumentos

---
+++ jenkins/jobscript2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#indicamos que pasaremos los argumentos para nombre y curso
+#ventaja se puede manipular desde jenkins
+nombre=$1
+curso=$2
+#Empezar el loop
+for a in 1 2 3 4 5 6 7 8 10 11 12 13 14 15
+do
+	#if es igual a 8 el loop descansa
+    if [ $a == 8 ]
+    then
+    	sleep 15
+        echo "A descansar de clase $nombre"
+    fi
+    echo "Clase No. $a"
+done
+sleep 30
+echo "Bien $nombre, terminamos las clases de $curso, nos vemos."
